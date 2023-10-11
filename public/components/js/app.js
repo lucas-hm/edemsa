3f791b6b3a4de5cf64525d94b8d19c1107a9bad2
lucas-hm <lucashugomoran12@gmail.com>
2023-10-11 09:13:29 -0300

    terminado

--- public/components/js/app.js
+++ public/components/js/app.js
@@ -1,23 +1,36 @@
-document.getElementById('submit').addEventListener('click', function () {
-    // Get form data
-    const fecha = document.querySelector('#fecha input[type="number"]').value;
-    const month = document.querySelector('select[name="month"]').value;
-    const days = document.querySelector('select[name="days"]').value;
-    const tipo = document.getElementById('tipo').value;
-    const garantia = document.getElementById('garantia').value;
-    const proveedor = document.getElementById('proveedor').value;
-    const uso = document.getElementById('uso').value;
-    const puesto = document.getElementById('puesto').value;
-    const observacion = document.getElementById('observacion').value;
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#register");
 
-    // Log the form data to the console
-    console.log('Fecha de compra:', fecha);
-    console.log('Month:', month);
-    console.log('Days:', days);
-    console.log('Tipo:', tipo);
-    console.log('Plazo de garantia:', garantia);
-    console.log('Proveedor:', proveedor);
-    console.log('Bien de uso:', uso);
-    console.log('Numero de puesto:', puesto);
-    console.log('Observaciones:', observacion);
-});
+  document.querySelector("#linkRegister").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
+  });
+
+  document.querySelector("#linkLogin").addEventListener("click", e => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      createAccountForm.classList.add("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", e => {
+      e.preventDefault();
+
+      // Perform your AJAX/Fetch login
+
+      setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+      document.querySelectorAll(".form__input").forEach(inputElement => {
+      inputElement.addEventListener("blur", e => {
+          if (e.target.id === "user" && e.target.value.length > 0 && e.target.value.length < 10) {
+              setInputError(inputElement, "Username must be at least 10 characters in length");
+          }
+      });
+
+      inputElement.addEventListener("input", e => {
+          clearInputError(inputElement);
+          });
+      });
+  });
