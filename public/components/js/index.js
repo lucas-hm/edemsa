ca5422742c34b5ce6ffb2db2165f970dcb9b89d4
lucas-hm <lucashugomoran12@gmail.com>
2023-10-17 10:14:35 -0300

    terminado

--- public/components/js/index.js
+++ public/components/js/index.js
@@ -1,13 +1,10 @@
 let API = "http://10.1.0.16:4000";
-const machineForm = document.getElementById('machineForm');
 
 let user = document.getElementById("user");
 let ID = document.getElementById("ID");
 let garantia = document.getElementById("garantia");
 let tipo = document.getElementById("tipo");
 let fecha = document.getElementById("fecha");
-let month = document.getElementById("fecha");
-let days = document.getElementById("fecha");
 let uso = document.getElementById("uso");
 let puesto = document.getElementById("puesto");
 let proveedor = document.getElementById("proveedor");
@@ -16,17 +13,15 @@ let observacion = document.getElementById("observacion");
 let btnSubmit = document.getElementById('btnSubmit');
 
 async function  handleSubmit(){
-  console.log(username.value)
+  console.log(fecha.value)
   const respuesta = await fetch(`${API}/machine`,{
     method:"POST",
   body: JSON.stringify ({
+    fecha:fecha.value,
     user:user.value,
     ID:ID.value,
     garantia:garantia.value,
     tipo:tipo.value,
-    fecha:fecha.value,
-    month:month.value,
-    days:days.value,
     uso:uso.value,
     puesto:puesto.value,
     proveedor:proveedor.value,
@@ -37,4 +32,4 @@ async function  handleSubmit(){
   console.log(res)
   }
 
-btnSubmit.addEventListener("click",btnSubmit)
+btnSubmit.addEventListener("click",handleSubmit)
