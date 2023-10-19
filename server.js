ca0802576e547876290332cb67b2a6191c9fa821
lucas-hm <lucashugomoran12@gmail.com>
2023-10-19 09:46:15 -0300

    terminado

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.get('/', (req, res)=> {
 });
 
 app.get('/listado', function (req, res) {
+  returnData().then(result => {
+    console.log(result); //This logs undefined
+    res.send(result); //This sends undefined
+  });
  const listado = path.resolve(__dirname, './public', './page', 'listado.html');
  res.sendFile(listado);
 });
@@ -62,3 +66,29 @@ mongoose.connect(URL, {
 app.listen(process.env.PORT, ()=> {
     console.log("Escuchando en http://10.1.0.16:4000");
 })
+
+async function returnData() {
+  const uri = "mongodb://root:example@10.1.0.16:27017/";
+
+  //Connect to the database and return all documents in the collection
+  const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  });
+  try {
+      await client.connect();
+      const database = client.db('data');
+      const collection = database.collection('edemsa');
+      const query = {};
+      const options = {};
+      const cursor = collection.find(query, options);
+      await cursor.toArray().then((docs) => {
+          console.log(docs); // <- This works and logs all the data to console 
+          return docs;
+      });
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
